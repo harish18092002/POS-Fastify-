71aadc554b59dc72d9359202fd393249f8ff487e
harish18092002 <harish18092002@gmail.com>
2024-05-27 10:08:46 +0530

    Update

--- src/app/plugins/sensible.ts
+++ src/app/plugins/sensible.ts
@@ -10,30 +10,3 @@ import sensible from '@fastify/sensible';
 export default fp(async function (fastify: FastifyInstance) {
   fastify.register(sensible);
 });
-
-// for (const tax of item.tax) {
-//   const existingTax = await ps.tax.findFirst({
-//     where: { itemId: itemId, taxType: tax.taxType },
-//   });
-
-//   if (existingTax) {
-//     arr.push(
-//       ps.tax.update({
-//         where: { taxId: existingTax.taxId },
-//         data: { taxAmount: tax.taxAmount },
-//       })
-//     );
-//   } else {
-//     arr.push(
-//       ps.tax.create({
-//         data: {
-//           taxId: generateID('HEX', '03'),
-//           orderId: oId,
-//           itemId: itemId,
-//           taxType: tax.taxType,
-//           taxAmount: tax.taxAmount,
-//         },
-//       })
-//     );
-//   }
-// }
