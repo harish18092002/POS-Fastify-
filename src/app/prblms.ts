a31cb4b4560c4a9df79634dc6155a37e1a272dfc
harish18092002 <harish18092002@gmail.com>
2024-05-31 17:16:13 +0530

    Fastify

--- src/app/prblms.ts
+++ src/app/prblms.ts
@@ -6,49 +6,49 @@ export function problems() {
   //   // console.log(str);
   // }
 
-  // // Problem 2
+  // // // Problem 2
   // for (let i = 1; i <= 100; i++) {
   //   // console.log(i);
   // }
 
-  // // Problem 3
+  // // // Problem 3
   // for (let i = 1; i <= 100; i++) {
   //   // if (i % 2 == 0) console.log('Even : ', i);
   //   // else console.log('Odd : ', i);
   // }
 
-  // // Problem 4
+  // // // Problem 4
   // const inputNumber = 45;
   // for (let i = inputNumber; i <= 100 + inputNumber; i++) {
   //   // console.log(i);
   // }
 
-  // // Problem 5
+  // // // Problem 5
   // let sum = 0;
   // for (let i = 1; i <= 100; i++) {
   //   sum = i + sum;
   // }
-  // console.log(sum);
+  // // console.log(sum);
 
-  // // Problem 6
+  // // // Problem 6
   // const words = 'abcdefghijklmnopqrstuvwxyz';
   // const alphabets = words.split(',').forEach((letters) => {
   //   console.log(letters);
   // });
 
-  // // Problem 7
+  // // // Problem 7
   // const userInput = 20;
   // for (let i = userInput; i <= 100 + userInput; i++) {
   //   if (i % 2 === 0) console.log('Even : ', i);
   //   else console.log('Odd : ', i);
   // }
 
-  // // Problem 8
+  // // // Problem 8
   // const arr = [1, 2, 3, 4, 5];
   // let add = arr.reduce((acc, red) => acc + red, 0);
   // console.log(add);
 
-  // // Problem 9
+  // // // Problem 9
 
   // const userValue = 45;
   // let sum1 = 0;
@@ -57,7 +57,7 @@ export function problems() {
   // }
   // console.log(sum1);
 
-  // Problem 10
+  // // Problem 10
   const pattern = 8;
   for (let i = 1; i <= 5; i++) {
     let line = '';
@@ -67,36 +67,81 @@ export function problems() {
     console.log(line);
   }
 
-  // // Problem 11
-  const num = '12345';
-  let sum2 = 0;
-  let i = 0;
-  while (i < num.length) {
-    sum2 = parseInt(num[i]) + sum2;
-    i++;
-  }
+  // // // Problem 11
+  // const num = '12345';
+  // let sum2 = 0;
+  // let i = 0;
+  // while (i < num.length) {
+  //   sum2 = parseInt(num[i]) + sum2;
+  //   i++;
+  // }
   // console.log('This is sum of the given num ' + sum2);
 
-  // // Problem 12
+  // // // Problem 12
 
-  const alphabets2 = 'abcdefghijklmnopqrstuvwxyz';
-  let j = 0;
-  while (j < alphabets2.length) {
-    const letters = alphabets2[j];
-    // console.log(letters);
-    j++;
-  }
+  // const alphabets2 = 'abcdefghijklmnopqrstuvwxyz';
+  // let j = 0;
+  // while (j < alphabets2.length) {
+  //   const letters = alphabets2[j];
+  //   // console.log(letters);
+  //   j++;
+  // }
 
   // Problem 13
 
-  const arr = [11, 12, 14];
-  const sumOfarr = arr.reduce((acc, red) => acc + red, 0).toString();
-  // console.log(sumOfarr);
-  let k = 0;
-  let sum = 0;
-  while (k < sumOfarr.length) {
-    sum += parseInt(sumOfarr[k]);
-    k++;
-  }
+  const arr = [10, 10, 19, 589, 90];
+  let sum = arr.reduce((acc, red) => acc + red, 0);
   console.log(sum);
+
+  while (sum.toString().length !== 1) {
+    sum = sum
+      .toString()
+      .split('')
+      .reduce((pre, cur) => pre + parseInt(cur), 0);
+    console.log(sum);
+  }
+  console.log({ sum });
+
+  // let k = 0;
+  // let sum = 0;
+  // const arr1 = [];
+  // while (k < sumOfarr.length) {
+  //   sum += parseInt(sumOfarr[k]);
+  //   k++;
+  // }
+  // console.log('This is arr1', sum.toString()[1]);
+  // for (let i = 0; i < sum.toString().length; i++) {
+  //   if (sum.toString().length > 1) {
+  //     arr1.push(parseInt(sum.toString()[i]));
+  //   }
+  // }
+  // const total = arr1.reduce((acc, red) => acc + red, 0);
+  // console.log('This is sum of the array', total);
+
+  // Problem 14
+
+  for (let i = 100; i <= 500; i++) {
+    const str = i.toString();
+    let l = 0;
+    const sum3 = [];
+    while (l < str.length) {
+      sum3.push(Math.pow(parseInt(str[l]), 3));
+      l++;
+    }
+    if (sum3.reduce((acc, red) => acc + red, 0) === i) {
+      console.log('These are the amstrong numbers:- ', i);
+    }
+  }
+  console.log();
+
+  // Problem 15
+  const arr2 = [9];
+  const input = 15;
+  let i = 0;
+  while (i < input - 1) {
+    arr2.push(arr2[i] * 10 + 9);
+    i++;
+  }
+  const total = arr2.reduce((prev, cur) => prev + cur, 0);
+  console.log(total);
 }
