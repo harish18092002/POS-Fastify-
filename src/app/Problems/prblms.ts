d2865094575a447ba14e7612abcf952635e924ad
harish18092002 <harish18092002@gmail.com>
2024-06-03 17:40:33 +0530

    COmmit

--- src/app/Problems/prblms.ts
+++ src/app/Problems/prblms.ts
@@ -1,55 +1,47 @@
-import { sep } from 'path';
+// import { sep } from 'path';
 
-// Problems
+// // Problems
 export function problems() {
   // Problem 1
   const str = 'I am a intern in Surfboard payments';
   for (let i = 1; i <= 7; i++) {
     console.log('Problem1 ', str);
   }
-
-  // Problem 2
+  //   // Problem 2
   for (let i = 1; i <= 100; i++) {
     console.log('Problem2 ', i);
   }
-
   // Problem 3
   for (let i = 1; i <= 100; i++) {
     if (i % 2 == 0) console.log('Even : ', i);
     else console.log('Odd : ', i);
   }
-
   // Problem 4
   const inputNumber = 45;
   for (let i = inputNumber; i <= 100 + inputNumber; i++) {
     console.log(i);
   }
-
   // Problem 5
   let sum4 = 0;
   for (let i = 1; i <= 100; i++) {
     sum4 = i + sum4;
   }
   console.log('Problem5 ', sum4);
-
   // Problem 6
   const words1 = 'abcdefghijklmnopqrstuvwxyz';
   const alphabets1 = words1.split(',').forEach((letters) => {
     console.log(letters);
   });
-
   // Problem 7
   const userInput1 = 20;
   for (let i = userInput1; i <= 100 + userInput1; i++) {
     if (i % 2 === 0) console.log('Even : ', i);
     else console.log('Odd : ', i);
   }
-
   // Problem 8
   const arr1 = [1, 2, 3, 4, 5];
   let add = arr1.reduce((acc, red) => acc + red, 0);
   console.log('Problem8 ', add);
-
   // Problem 9
   const userValue = 45;
   let sum1 = 0;
@@ -57,7 +49,6 @@ export function problems() {
     sum1 = sum1 + i;
   }
   console.log('Problem9 ', sum1);
-
   // Problem 10
   const pattern = 8;
   for (let i = 1; i <= 5; i++) {
@@ -67,7 +58,6 @@ export function problems() {
     }
     console.log(line);
   }
-
   // Problem 11
   const num = '12345';
   let sum3 = 0;
@@ -77,7 +67,6 @@ export function problems() {
     l++;
   }
   console.log('This is sum of the given num ' + sum3);
-
   // Problem 12
   const alphabets2 = 'abcdefghijklmnopqrstuvwxyz';
   let j = 0;
@@ -86,12 +75,10 @@ export function problems() {
     console.log(letters);
     j++;
   }
-
   // Problem 13
   const arr = [10, 10, 19, 589, 90];
   let sum = arr.reduce((acc, red) => acc + red, 0);
   console.log(sum);
-
   while (sum.toString().length !== 1) {
     sum = sum
       .toString()
@@ -100,7 +87,6 @@ export function problems() {
     console.log(sum);
   }
   console.log({ sum });
-
   // Problem 14
   for (let i = 100; i <= 500; i++) {
     const str = i.toString();
@@ -114,22 +100,17 @@ export function problems() {
       l++;
     }
   }
-
-  // Problem 15
-
+  //   // Problem 15
   const input = 5;
   let sum6 = 0;
   let i = 1;
   while (i <= input) {
     console.log(i);
     sum6 = sum6 + (Math.pow(10, i) - 1);
-
     i++;
   }
   console.log('This is the total of the problem 15', sum6);
-
   // Problem 16
-
   const primes = [];
   for (let i = 2; i < 100; i++) {
     let isPrime = true;
@@ -144,9 +125,7 @@ export function problems() {
     }
   }
   console.log(primes);
-
   // Problem 17
-
   const userInput = 'sam';
   const alphabets = 'abcdefghijklmnopqrstuvwxyz';
   const arr5 = [];
@@ -160,16 +139,12 @@ export function problems() {
   }
   const total16 = arr5.reduce((prev, cur) => prev + cur, 0);
   console.log(total16);
-
   // Problem 18
-
   const passPhrase = 'aaa aa bb cc dd ee';
   const words = passPhrase.split(' ');
   console.log('This is split', words);
-
   let wordCount = {};
   let isValid = true;
-
   for (let i = 0; i < words.length; i++) {
     console.log(words[i]);
     if (wordCount[words[i]]) {
@@ -180,33 +155,26 @@ export function problems() {
       wordCount[words[i]] = 1;
     }
   }
-
   if (isValid) {
     console.log(wordCount, ' This is words count');
     console.log('The pass phrase is valid');
   } else {
     console.log('The pass phrase is not valid');
   }
-
   // Problem 19
   const captcha = '1122211';
-
   let sum2 = 0;
   const lengt = captcha.length;
-
   for (let i = 0; i < lengt; i++) {
     const currentDigit = captcha[i];
     const nextDigit = captcha[(i + 1) % lengt];
     console.log(nextDigit);
-
     if (currentDigit === nextDigit) {
       sum2 += parseInt(currentDigit);
     }
   }
   console.log('This is problem 19', sum2);
-
   // Problem 20
-
   const matrix = [
     [5, 1, 9, 5],
     [5, 7, 3],
@@ -214,14 +182,12 @@ export function problems() {
   ];
   console.log(matrix.length);
   let sum5 = 0;
-
   for (let i = 0; i < matrix.length; i++) {
     const values = matrix[i];
     console.log(values);
     let min = values[0];
     console.log(min);
     let max = values[0];
-
     for (let j = 1; j < values.length; j++) {
       if (values[j] < min) {
         min = values[j];
@@ -234,7 +200,7 @@ export function problems() {
   }
   console.log('Sum of problem 20:', sum5);
 
-  // Splitting a string without using split inbuild function
+  //   // Splitting a string without using split inbuild function
 
   const st = 'abbbb';
   console.log(st.length);
