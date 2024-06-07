1c41c363ac37da6ebc4317b8d1d7d5d6138dcad5
harish18092002 <harish18092002@gmail.com>
2024-06-07 10:19:45 +0530

    Commit

--- src/app/Problems/prblms.ts
+++ src/app/Problems/prblms.ts
@@ -507,7 +507,6 @@ for (let i = 0; i < num1 - 2; i++) {
 // console.log(arr8);
 
 // 16
-
 const matrix1 = [
   [1, 2, 3],
   [4, 5, 6],
@@ -549,3 +548,37 @@ else {
     sum
   );
 }
+
+// 17
+// Multiplication
+const matrix3 = [
+  [1, 2, 3],
+  [4, 5, 4],
+  [4, 5, 5],
+];
+
+const matrix4 = [
+  [4, 5, 6],
+  [1, 2, 7],
+  [4, 5, 8],
+];
+let result = [];
+
+for (let i = 0; i < matrix3[0].length; i++) {
+  result[i] = [];
+  console.log(result);
+  for (let j = 0; j < matrix4.length; j++) {
+    result[i][j] = 0;
+    console.log(result);
+  }
+}
+
+for (let i = 0; i < matrix3.length; i++) {
+  for (let j = 0; j < matrix4[0].length; j++) {
+    for (let k = 0; k < matrix4.length; k++) {
+      result[i][j] += matrix3[i][k] * matrix4[k][j];
+    }
+  }
+}
+
+console.log(result);
