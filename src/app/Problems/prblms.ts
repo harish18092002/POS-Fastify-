3c64d3583ba74602f5057ab4d0db5e59cbb2c302
harish18092002 <harish18092002@gmail.com>
2024-06-07 16:41:38 +0530

    Commit

--- src/app/Problems/prblms.ts
+++ src/app/Problems/prblms.ts
@@ -717,7 +717,22 @@ for (let i = 0; i < printerCode.length; i++) {
     if (printerCode[i] === validCode[j]) {
       break;
     } else {
+      if (j === validCode.length - 1) error++;
     }
   }
 }
 console.log('Printer Error = ', error, '/', printerCode.length);
+
+// Problem 4
+
+// Take 2 strings s1 and s2 including only letters from a to z. Return
+// a new sorted string, the longest possible, containing distinct letters - each
+//  taken only once - coming from s1 or s2.
+
+// Examples:
+// a = "xyaabbbccccdefww"
+// b = "xxxxyyyyabklmopq"
+// longest(a, b) -> "abcdefklmopqwxy"
+
+// a = "abcdefghijklmnopqrstuvwxyz"
+// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
