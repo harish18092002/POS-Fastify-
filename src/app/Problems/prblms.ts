5f87582f32ea76e9ed86c29a8796c30c4da73bc9
harish18092002 <harish18092002@gmail.com>
2024-06-08 22:27:14 +0530

    Commit

--- src/app/Problems/prblms.ts
+++ src/app/Problems/prblms.ts
@@ -630,11 +630,11 @@ if (matrix5.length != matrix6.length) {
 // Pattern
 const pattern = '*';
 for (let i = 5; i >= 1; i--) {
-  let line = '';
+  let newline = '';
   for (let j = 1; j <= i; j++) {
-    line = line + pattern;
+    newline = newline + pattern;
   }
-  console.log(line);
+  console.log(newline);
 }
 
 // Stone paper scissor problem
@@ -788,3 +788,18 @@ else {
   const j = Math.floor(input5.length / 2);
   console.log(input5.slice(j, j + 1));
 }
+
+// Problem 7
+
+// After a hard quarter in the office you decide to get some rest on a vacation.
+//  So you will book a flight for you and try to leave all the
+//  mess behind you.
+
+// You will need a rental car in order for you to get around in your vacation.
+//  The manager of the car rental makes you some good offers.
+
+// Every day you rent the car costs $40. If you rent the car for 7 or more days,
+// you get $50 off your total. Alternatively, if you rent the car for 3 or more days,
+//  you get $20 off your total.
+
+// Write a code that gives out the total amount for different days(d).
